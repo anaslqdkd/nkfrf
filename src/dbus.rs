3c0e2925cb8737fd010b8a364504525a2aad41fe
ash <anaa.ceban@gmail.com>
2025-12-10 15:39:36 +0100

    refactoring

---
+++ src/dbus.rs
@@ -0,0 +1,83 @@
+use std::future::pending;
+
+use zbus::zvariant::{OwnedValue, Value};
+use zbus::{connection, interface, zvariant};
+#[derive(Debug, Clone)]
+pub struct Hints {
+    pub urgency: u8,
+}
+#[derive(Debug, Clone)]
+pub struct NotificationData {
+    pub summary: String,
+    pub body: String,
+    pub icon: String,
+    pub actions: Vec<String>,
+    pub hints: Hints,
+    pub expire_timeout: i32,
+}
+#[derive(Debug, Clone)]
+pub struct NotificationService {
+    count: u64,
+    sender: glib::Sender<NotificationData>,
+}
+#[interface(name = "org.freedesktop.Notifications")]
+impl NotificationService {
+    fn say_hello(&mut self, name: &str) -> String {
+        self.count += 1;
+        format!("Hello {}! I have been called {} times.", name, self.count)
+        
+    }
+    fn get_server_information(&self) -> (&str, &str, &str, &str) {
+        (
+            "NotifDaemon", 
+            "Me",          
+            "0.0",         
+            "0.0",         
+        )
+    }
+    fn notify( &self, app_name: &str, replaces_id: u32, app_icon: &str, summary: &str, body: &str, actions: Vec<String>, hints: std::collections::HashMap<String, zvariant::Value>, expire_timeout: i32,) -> u32 {
+        println!("the notification is app_name{}, body{}", app_name, body);
+        let summary = summary.to_string();
+        let body = body.to_string();
+        let icon = app_icon.to_string();
+        let hints_struct = Hints {
+            urgency: match hints.get("urgency") {
+                Some(Value::U8(u)) => *u,
+                _ => 1, 
+            },
+        };
+        let mut duration = 5000;
+        if expire_timeout != -1{
+            duration = expire_timeout;
+        }
+
+        let notification = NotificationData {
+            summary: summary,
+            body: body,
+            icon: icon,
+            actions: actions,
+            expire_timeout: duration,
+            hints: hints_struct,
+            
+        };
+        println!("the hints are: {:#?}", hints);
+        self.sender.send(notification).expect("Pb with the send notif");
+        1
+    }
+    pub async fn show_nc(&self){
+        println!("The interface executed the show nc");
+    }
+}
+pub async fn run(sender: glib::Sender<NotificationData>) -> anyhow::Result<()> {
+    println!("Done!");
+    let greeter = NotificationService {count:0, sender};
+    let _conn = connection::Builder::session().expect("Pb here")
+        .name("org.freedesktop.Notifications").expect("Pb name")
+        .serve_at("/org/freedesktop/Notifications", greeter).expect("Pb servve at")
+        .build()
+        .await;
+
+    pending::<()>().await;
+    Ok(())
+
+}
--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,17 @@
+use anyhow::Ok;
+use gtk4::{Application, ApplicationWindow};
+use clap::{Parser, Subcommand, ValueEnum};
 use std::cell::{Cell, RefCell};
-use std::future::pending;
 use std::rc::Rc;
-use std::time::SystemTime;
-use notify_rust::Notification;
-use std::collections::VecDeque;
+use zbus::zvariant::{OwnedValue, Value};
+use zbus::{Connection, proxy};
+use std::collections::{HashMap, VecDeque};
 use pango;
-
-use gtk4::ffi::gtk_label_new;
 use gtk4::gdk::Display;
 use gtk4::{prelude::*, Button, CssProvider, Label, STYLE_PROVIDER_PRIORITY_APPLICATION};
-use gtk4::{Application, ApplicationWindow};
-use glib::{timeout_add_local, MainContext, Priority};
-// use zbus::blocking::connection;
+use glib::{timeout_add_local, ControlFlow, MainContext, Priority};
 use gtk4_layer_shell::{Edge, Layer, LayerShell};
-use zbus::{connection, interface, zvariant};
-use lipsum::lipsum;
+mod dbus;
 
 const TOP_MARGIN: i32 = 20;
 const WINDOW_HEIGHT: i32 = 70;
@@ -23,41 +20,64 @@ const MAX_ACTIVE: i32 = 5;
 
 type WindowList = Rc<RefCell<Vec<ApplicationWindow>>>;
 
-struct NotificationService {
-    count: u64,
-    sender: glib::Sender<NotificationData>,
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Cli {
+    #[command(subcommand)]
+    command: Option<Commands>,
 }
+#[derive(Debug, Subcommand)]
+enum Commands {
+    Show,
+}
+// TODO: implement critical/normal priorities
 
-#[derive(Debug, Clone)]
-struct NotificationData {
-    summary: String,
-    body: String,
+fn show_notification_popup( queue: &Rc<RefCell<VecDeque<dbus::NotificationData>>>, app: &Application, active_notifications: &Rc<RefCell<i32>>, active_windows: &WindowList){
+    if *active_notifications.borrow() >= MAX_ACTIVE {
+        return;
+    }
+    println!("in show notification popup");
+    let mut q = queue.borrow_mut();
+    if let Some(notification) = q.pop_front(){
+        // is_showing.set(true);
+        let summary: &str = &notification.summary;
+        let body: &str = &notification.body;
+        let icon: &str = &notification.icon;
+        let actions: &Vec<String> = &notification.actions;
+        let expire_timeout: i32 = notification.expire_timeout;
+        let hints: u8 = notification.hints.urgency;
+        println!("In the if statement with notification {:#?}", notification);
+        println!("actions = {:?}", actions);
+        let window = draw_window(summary, body, app, *active_notifications.borrow_mut());
+        *active_notifications.borrow_mut() += 1;
+        window.show();
+        active_windows.borrow_mut().push(window.clone());
+        let queue = queue.clone();
+        let window = window.clone();
+        let app_clone = app.clone();
+        let active_notifications = active_notifications.clone();
+        let active_windows = active_windows.clone();
+        let duration = expire_timeout as u64;
+
+        timeout_add_local(std::time::Duration::from_millis(duration), move || {
+            println!("The time is up");
+            println!("Queue: {:?}", queue.borrow());
+            *active_notifications.borrow_mut() -= 1;
+            active_windows.borrow_mut().retain(|w| !w.eq(&window));
+            window.hide();
+            redraw_windows(&active_windows);
+            show_notification_popup(&queue, &app_clone, &active_notifications, &active_windows);
+            glib::ControlFlow::Break
+        });
+    }
 }
-#[interface(name = "org.freedesktop.Notifications")]
-impl NotificationService {
-    fn say_hello(&mut self, name: &str) -> String {
-        self.count += 1;
-        format!("Hello {}! I have been called {} times.", name, self.count)
-    }
-    fn get_server_information(&self) -> (&str, &str, &str, &str) {
-        (
-            "NotifDaemon", 
-            "Me",          
-            "0.0",         
-            "0.0",         
-        )
-    }
-    fn notify( &self, app_name: &str, replaces_id: u32, app_icon: &str, summary: &str, body: &str, actions: Vec<String>, hints: std::collections::HashMap<String, zvariant::Value>, expire_timeout: i32,) -> u32 {
-        println!("the notification is app_name{}, body{}", app_name, body);
-        let summary = summary.to_string();
-        let body = body.to_string();
-        let icon = app_icon.to_string();
-        let notification = NotificationData {
-            summary: summary.to_string(),
-            body: body.to_string()
-        };
-        self.sender.send(notification).expect("Pb with the send notif");
-        1
+fn redraw_windows(windows: &WindowList) {
+    let mut index: i32 = 0;
+
+    for win in windows.borrow().iter() {
+        let y_offset = index*WINDOW_HEIGHT + TOP_MARGIN;
+        win.set_margin(Edge::Top, y_offset);
+        index += 1;
     }
 }
 fn draw_window (summary: &str, body: &str, app: &Application, stack_number: i32) -> ApplicationWindow{
@@ -70,7 +90,6 @@ fn draw_window (summary: &str, body: &str, app: &Application, stack_number: i32)
         .build();
     window.init_layer_shell();
     window.set_layer(Layer::Overlay);
-    // TODO: make it adapt to size based on the display
     window.set_default_size(WINDOW_WIDTH, WINDOW_HEIGHT);
     window.set_opacity(0.95);
     let anchors = [
@@ -123,128 +142,51 @@ fn draw_window (summary: &str, body: &str, app: &Application, stack_number: i32)
     window
 
 }
-fn show_notification_popup( queue: &Rc<RefCell<VecDeque<NotificationData>>>, is_showing: &Rc<Cell<bool>>, app: &Application, active_notifications: &Rc<RefCell<i32>>, active_windows: &WindowList){
-    if *active_notifications.borrow() >= MAX_ACTIVE {
-        return;
-    }
-    println!("in show notification popup");
-    let mut q = queue.borrow_mut();
-    if let Some(notification) = q.pop_front(){
-        // is_showing.set(true);
-        let summary: &str = &notification.summary;
-        let body: &str = &notification.body;
-        println!("In the if statement with summary {} body {}", summary,body);
-        let window = draw_window(summary, body, app, *active_notifications.borrow_mut());
-        *active_notifications.borrow_mut() += 1;
-        window.show();
-        active_windows.borrow_mut().push(window.clone());
-        let queue = queue.clone();
-        let is_showing = is_showing.clone();
-        let window = window.clone();
-        let app_clone = app.clone();
-        let active_notifications = active_notifications.clone();
-        let active_windows = active_windows.clone();
-        timeout_add_local(std::time::Duration::from_secs(10), move || {
-            println!("The time is up");
-            println!("Queue: {:?}", queue.borrow());
-            *active_notifications.borrow_mut() -= 1;
-            active_windows.borrow_mut().retain(|w| !w.eq(&window));
-            window.hide();
-            redraw_windows(&active_windows);
-            show_notification_popup(&queue, &is_showing, &app_clone, &active_notifications, &active_windows);
-            glib::ControlFlow::Break
-        });
-    }
-}
-    //     // TODO: spaw windows until there is notification and until there are items in the queue
-    //     // TODO: implement critical/normal priorities
-fn redraw_windows(windows: &WindowList) {
-    let mut index: i32 = 0;
 
-    for win in windows.borrow().iter() {
-        let y_offset = index*WINDOW_HEIGHT + TOP_MARGIN;
-        win.set_margin(Edge::Top, y_offset);
-        index += 1;
-    }
+fn show_nc() -> Result<(), anyhow::Error>{
+    println!("Launched with the show option");
+    Ok(())
 }
 
-#[tokio::main] 
-async fn main() {
-	let app = Application::new(
-		Some("com.example.asyncgtk"),
-		Default::default(),
-	);
 
-	app.connect_activate(|app| {
-        let queue = Rc::new(RefCell::new(VecDeque::<NotificationData>::new()));
-        let queue_for_receiver = queue.clone();
-        let active_notifications = Rc::new(RefCell::new(0));
-        let window = Rc::new(ApplicationWindow::builder()
-            .application(app)
-            .title("Async GTK Example")
-            .default_width(300)
-            .default_height(100)
-            .build());
-            window.init_layer_shell();
-            window.set_layer(Layer::Overlay);
-            // TODO: make it adapt to size based on the display
-            window.set_default_size(270, 70);
-            window.set_opacity(0.95);
-        let anchors = [
-            (Edge::Left, false),
-            (Edge::Right, true),
-            (Edge::Top, true),
-            (Edge::Bottom, false),
-        ];
-        for (anchor, state) in anchors {
-            window.set_anchor(anchor, state);
-        }
-        let provider = CssProvider::new();
-        provider.load_from_path("style.css");
-        let display = Display::default().expect("Could not connect to display");
-        gtk4::style_context_add_provider_for_display(
-            &display,
-            &provider,
-            STYLE_PROVIDER_PRIORITY_APPLICATION,
-        );
-        let container = gtk4::Box::new(gtk4::Orientation::Vertical, 0);
-        container.add_css_class("container-bg");
+#[tokio::main]
+async fn main() -> Result<(), anyhow::Error> {
+    let (sender, receiver) = glib::MainContext::channel::<dbus::NotificationData>(glib::Priority::DEFAULT);
 
-
-        window.set_child(Some(&container));
-
-        window.set_margin(Edge::Right, 20);
-        window.set_margin(Edge::Top, 20);
-        // NOTE: deprecated but works
-        let (sender, receiver) = glib::MainContext::channel::<NotificationData>(glib::Priority::DEFAULT);
-        let is_showing = Rc::new(Cell::new(false));
-        let is_showing_clone = is_showing.clone();
-        let app_clone = app.clone();
-        let active_windows: WindowList = Rc::new(RefCell::new(Vec::new()));
-        let active_windows = active_windows.clone();
-        receiver.attach(None, move |notification| {
-            println!("in the receiver attach");
-            queue_for_receiver.borrow_mut().push_back(notification.clone());
-            show_notification_popup(&queue_for_receiver, &is_showing_clone.clone(), &app_clone, &active_notifications, &active_windows);
-            // if !(is_showing.get()){
-            // }
-            glib::ControlFlow::Continue
-        });
-
-		MainContext::default().spawn_local(async move {
-
-			println!("Done!");
-            let greeter = NotificationService {count:0, sender};
-            let _conn = connection::Builder::session().expect("Pb here")
-                .name("org.freedesktop.Notifications").expect("Pb name")
-                .serve_at("/org/freedesktop/Notifications", greeter).expect("Pb servve at")
-                .build()
-                .await;
-
-            pending::<()>().await;
-		});
-
+    tokio::spawn(async move {
+        dbus::run(sender).await.unwrap();
     });
-	app.run();
-}
+    let cli = Cli::parse();
+    let app = Application::new(
+        Some("com.example.asyncgtk"),
+        Default::default(),
+    );
+    app.connect_activate(move |app| {
+        let window = ApplicationWindow::builder().application(app).build();
+        window.hide();
+    });
 
+
+    if let Some(cmd) = &cli.command {
+        match cmd {
+            Commands::Show => show_nc()?,
+        }
+    } 
+
+    let app_clone = app.clone();
+    let queue = Rc::new(RefCell::new(VecDeque::<dbus::NotificationData>::new()));
+    let active_notifications = Rc::new(RefCell::new(0));
+    let active_windows: WindowList = Rc::new(RefCell::new(Vec::new()));
+    let active_windows = active_windows.clone();
+    receiver.attach(None, move |notification| {
+        println!("in the receiver attach");
+        queue.borrow_mut().push_back(notification.clone());
+        show_notification_popup(&queue, &app_clone, &active_notifications, &active_windows);
+        glib::ControlFlow::Continue
+    });
+
+    
+    app.run();
+
+    Ok(())
+}
